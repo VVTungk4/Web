ebd96dc62de97dab8a438e452fbb3088a1e2b8bc
Duong-Duc-Khoi <150509827+Duong-Duc-Khoi@users.noreply.github.com>
2024-06-14 19:03:36 +0700

    "chỉnh sửa lại form admin"

--- admin/script.js
+++ admin/script.js
@@ -73,3 +73,24 @@ switchMode.addEventListener('change', function () {
 		document.body.classList.remove('dark');
 	}
 })
+//show content
+function showContent(contentId) {
+    // Ẩn tất cả các phần nội dung
+    var contents = document.getElementsByClassName('content');
+    for (var i = 0; i < contents.length; i++) {
+        contents[i].style.display = 'none';
+    }
+
+    // Hiển thị phần nội dung được chọn
+    document.getElementById(contentId).style.display = 'block';
+}
+function showSanpham(sanphamId) {
+    // Ẩn tất cả các phần nội dung
+    var sanpham = document.getElementsByClassName('sanpham');
+    for (var i = 0; i < sanpham.length; i++) {
+        sanpham[i].style.display = 'none';
+    }
+
+    // Hiển thị phần nội dung được chọn
+    document.getElementById(sanphamId).style.display = 'block';
+}
