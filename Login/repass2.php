0fef3d90b6deabdec0bb9025bd26fb59ba3fd5d6
Duong-Duc-Khoi <150509827+Duong-Duc-Khoi@users.noreply.github.com>
2024-06-18 08:57:11 +0700

    m

--- Login/repass2.php
+++ Login/repass2.php
@@ -11,41 +11,40 @@ if (isset($_POST['oce'])) {
     if (isset($_SESSION['email']) && $_SESSION['otp']) {
         $email = $_SESSION['email'];
         $otp = $_SESSION['otp'];
-//
+        //
 
-//
-    if ($password != $repass) {
+        //
+        if ($password != $repass) {
 
-        echo '<script language="javascript">alert("Không trùng password!" echo $otp;); window.location="../Login/repass.html";</script>';
-        die();
-    } else {
-        if ($otp_n != $otp) {
-            echo $otp;
-
+            echo '<script language="javascript">alert("Không trùng password!" echo $otp;); window.location="../Login/repass.html";</script>';
             die();
         } else {
-            $conn =  mysqli_connect('localhost', 'root', '') or die("Lỗi kết nối");
-
-            mysqli_select_db($conn, 'webhangban') or die('Not find DataBase');
-            // Dùng isset để kiểm tra Form
-
-            // Tiếp tục xử lý với OTP
-            $sql = "UPDATE user SET password=? WHERE email=?";
-            $stmt = $conn->prepare($sql);
-            //
-            if ($stmt !== false) {
-                $stmt->bind_param("ss", $password, $email);
-                $stmt->execute();
-                session_unset();
-                session_destroy();
-                header('location: ../Login/Login.html');
-                exit();
+            if ($otp_n != $otp) {
+                echo '<script language="javascript">alert("Không trùng password!" echo $otp;); window.location="../Login/repass.html";</script>';
+                die();
             } else {
-                // Xử lý lỗi
-                echo "Lỗi khi chuẩn bị câu lệnh: " . $conn->error;
+                $conn = mysqli_connect('localhost', 'root', '') or die("Lỗi kết nối");
+
+                mysqli_select_db($conn, 'webhangban') or die('Not find DataBase');
+                // Dùng isset để kiểm tra Form
+
+                // Tiếp tục xử lý với OTP
+                $sql = "UPDATE user SET password=? WHERE email=?";
+                $stmt = $conn->prepare($sql);
+                //
+                if ($stmt !== false) {
+                    $stmt->bind_param("ss", $password, $email);
+                    $stmt->execute();
+                    session_unset();
+                    session_destroy();
+                    header('location: ../Login/Login.html');
+                    exit();
+                } else {
+                    // Xử lý lỗi
+                    echo "Lỗi khi chuẩn bị câu lệnh: " . $conn->error;
+                }
+                $conn->close();
             }
-            $conn->close();
         }
     }
-}
 }
