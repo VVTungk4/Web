83ca822f3fb6b091c30841ca3ab44b5a8426c284
Tang Tuan <tangtuann96@gmail.com>
2024-06-22 17:57:25 +0700

    len

--- demoSearch/view_search.php
+++ demoSearch/view_search.php
@@ -6,7 +6,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Chi tiết sản phâm</title>
-    <link rel="stylesheet" href="search_sp.css">
     <link rel="icon" type="image/png" href="../sanpham/image/sp.ico" />
     <link rel="stylesheet" href="./themify-icons/themify-icons.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
