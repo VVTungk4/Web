6256bbf214f7f191ac055d0d67c3616b58ae317f
Tang Tuan <tangtuann96@gmail.com>
2024-07-09 17:53:07 +0700

    continiu

--- php/get_order_info.php
+++ php/get_order_info.php
@@ -9,16 +9,19 @@
         $fullName = $_POST['billing_address_full_name'];
         $phone = $_POST['billing_address_phone'];
         $address = $_POST['billing_address_address'];
+        $note =  $_POST['billing_address_note'];
 
         // In ra các giá trị dữ liệu nhận được từ form
         echo "Full Name: " . $fullName . "<br>";
         echo "Phone: " . $phone . "<br>";
         echo "Address: " . $address . "<br>";
+        echo "Note: " . $note . "<br>";
 
         // Lưu thông tin vào session
         $_SESSION['billing_address_full_name'] = $fullName;
         $_SESSION['billing_address_phone'] = $phone;
         $_SESSION['billing_address_address'] = $address;
+        $_SESSION['billing_address_note'] = $note;
 
         // Chuyển hướng hoặc thực hiện các hành động khác sau khi lưu thông tin vào session
         // Ví dụ:
